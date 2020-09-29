31aeac5b29031dc06c5cc57fc321e3f69e482a34
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-09-29 06:57:57 +0100

    init week 4

---
+++ part-one/week-3/Lecture.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+package PACKAGE_NAME;
+
+public class Lecture {
+    public static void main(String[] args) {
+
+    }
+}
